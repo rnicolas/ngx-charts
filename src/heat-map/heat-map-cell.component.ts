adbe149d8664e00fd78bc24a410ad067aa97e0a4
Roger Nicolás Alegret <roger.nicolas@aeinnova.com>
2017-07-11 11:51:31 +0200

    styling

--- src/heat-map/heat-map-cell.component.ts
+++ src/heat-map/heat-map-cell.component.ts
@@ -33,6 +33,15 @@ import { id } from '../utils/id';
         style="cursor: pointer"
         (click)="onClick()"
       />
+	  <svg:text
+		  text-anchor="middle"
+		  [attr.x]="50%"
+		  [attr.y]="50%"
+		  [attr.width]="width"
+		  [attr.height]="height"
+		  >
+	  Just a test
+	  </svg:text>
     </svg:g>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
